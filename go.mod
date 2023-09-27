afeaac656dc87bae35d1480030e023f911494c24
Manas Mishra <manas.m22@gmail.com>
2023-09-27 14:20:45 +0530

    Refactored the code

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/manasm11/yt-todolist
 
 go 1.21.1
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require github.com/julienschmidt/httprouter v1.3.0
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -15,13 +15,33 @@ type todoApiServeMux struct {
 func NewTodoApiServeMux() (s *todoApiServeMux) {
 	s = new(todoApiServeMux)
 	s.r = httprouter.New()
-	s.r.HandlerFunc(http.MethodGet, "/todos/", func(w http.ResponseWriter, r *http.Request) {})
-	s.r.HandlerFunc(http.MethodPost, "/todo/", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusCreated) })
-	s.r.HandlerFunc(http.MethodGet, "/todo/:id", func(w http.ResponseWriter, r *http.Request) {})
-	s.r.HandlerFunc(http.MethodPut, "/todo/:id", func(w http.ResponseWriter, r *http.Request) {})
-	s.r.HandlerFunc(http.MethodDelete, "/todo/:id", func(w http.ResponseWriter, r *http.Request) {})
+	s.r.HandlerFunc(http.MethodGet, "/todos/", s.handleAllTodos)
+	s.r.HandlerFunc(http.MethodPost, "/todo/", s.handleCreateTodo)
+	s.r.HandlerFunc(http.MethodGet, "/todo/:id", s.handleGetTodo)
+	s.r.HandlerFunc(http.MethodPut, "/todo/:id", s.handleUpdateTodo)
+	s.r.HandlerFunc(http.MethodDelete, "/todo/:id", s.handleDeleteTodo)
 	// s.td = tododao
 	return
+}
+
+func (s todoApiServeMux) handleAllTodos(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s todoApiServeMux) handleCreateTodo(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (s todoApiServeMux) handleGetTodo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s todoApiServeMux) handleUpdateTodo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s todoApiServeMux) handleDeleteTodo(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func (s todoApiServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
